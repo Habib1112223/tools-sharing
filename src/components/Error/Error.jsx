f1b6debbd08f48bc75866c3a94452efe33a0656f
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2023-11-08 18:49:41 +0600

    Error page button set up in the website page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,2 +1,16 @@
 import { Link } from "react-router-dom";
 import img from '../../assets/image/404 image.jpg'
+
+
+const Error = () => {
+      return (
+            <div>
+                  <img src={img} alt="404 image" />
+                  <div className=" flex justify-center items-center ">
+                        <button><Link className="text-white btn btn-primary mt-8" to="/">Back to Home</Link> </button>
+                  </div>
+            </div>
+      );
+};
+
+export default Error;
